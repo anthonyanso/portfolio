a76b3270e44e2be5c04c40244b3d86b4cd7a4f22
Anthony <anthonyanso2@gmail.com>
2025-07-10 10:02:19 +0100

    Update project: latest edits and fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import ClientRootLayout from '@/components/ClientRootLayout';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   title: 'Anthony Anso | Portfolio',
@@ -15,6 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <ClientRootLayout>
         {children}
+        <SpeedInsights />
       </ClientRootLayout>
     </html>
   );
